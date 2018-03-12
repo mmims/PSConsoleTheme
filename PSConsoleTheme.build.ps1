8093ccc75f61ca76c037969520d367de85361ee9
Michael Mims <mims.michael@gmail.com>
2018-03-11 21:04:13 -0400

    Remove binary build task.

--- PSConsoleTheme.build.ps1
+++ PSConsoleTheme.build.ps1
@@ -4,41 +4,9 @@ param (
     [string]$NuGetApiKey = (property 'NuGetApiKey' '')
 )
 
-use * MSBuild
-
-function New-DebugTarget ([int]$length = 8) {
-    do {
-        $ranDir = -join ((48..57) + (97..122) | Get-Random -Count $length | ForEach-Object { [char]$_ })
-        $newDir = Join-Path $BuildRoot "module/$Configuration/$ranDir/PSConsoleTheme"
-    } while (Test-Path $newDir)
-    $newDir
-}
-
 $targetDir = Join-Path $BuildRoot "module/$Configuration/PSConsoleTheme"
 if ($Configuration -eq 'Debug') {
-    if ($Global:PSConsoleThemeDebugSessionPath -and (Test-Path $Global:PSConsoleThemeDebugSessionPath)) {
-        $targetDir = $Global:PSConsoleThemeDebugSessionPath
-    } else {
-        $Global:PSConsoleThemeDebugSessionPath = $targetDir = New-DebugTarget
-    }
-}
-
-$binaryModuleParams = @{
-    Inputs = { Get-ChildItem PSConsoleTheme/Lib/*.cs, PSConsoleTheme/Lib/Properties/*.cs, PSConsoleTheme/Lib/PSConsoleTheme.csproj, PSConsoleTheme/Lib/PSConsoleTheme.sln }
-    Outputs = "PSConsoleTheme/Lib/bin/$Configuration/PSConsoleTheme.dll"
-}
-
-# Synopsis: Build the binary library used by the module
-task BuildBinaryModule @binaryModuleParams {
-    exec { MSBuild 'PSConsoleTheme/Lib/PSConsoleTheme.csproj' /t:Rebuild /p:Configuration=$Configuration /p:Platform=AnyCPU }
-
-    $manifestFile = 'PSConsoleTheme/PSConsoleTheme.psd1'
-    $version = (Get-ChildItem "PSConsoleTheme/Lib/bin/$Configuration/PSConsoleTheme.dll").VersionInfo.FileVersion
-    $manifestContent = Get-Content -Path $manifestFile -Raw
-
-    $manifestContent = [regex]::Replace($manifestContent, "ModuleVersion = '.*'", "ModuleVersion = '$version'")
-    Write-Host "Updating version information in manifest: $manifestFile"
-    $manifestContent | Set-Content -Path $manifestFile -Encoding UTF8 -NoNewline
+    $Global:PSConsoleThemeDebugSessionPath = $targetDir
 }
 
 $mamlHelpParams = @{
@@ -46,21 +14,22 @@ $mamlHelpParams = @{
     Outputs = (Join-Path $targetDir 'en-US/PSConsoleTheme-help.xml')
 }
 
+# Synopsis: Build external help file from markdown documentation
 task BuildMamlHelp @mamlHelpParams -If ($Configuration -eq 'Release') {
     PlatyPS\New-ExternalHelp -Path docs -OutputPath $targetDir\en-US\PSConsoleTheme-help.xml -Force
 }
 
 # Synopsis: Remove all build related artifacts
 task Clean {
-    Get-ChildItem PSConsoleTheme/Lib -Include bin, obj -Recurse | Remove-Item -Recurse -Force -ErrorAction Ignore
     Remove-Item module -Recurse -Force -ErrorAction Ignore
 }
 
+# Synopsis: Import the module for use in the current session
 task Install LayoutModule, {
     switch ($Configuration) {
         Debug {
             Remove-Module PSConsoleTheme -Force -ErrorAction Ignore
-            Import-Module (Join-Path ${Global:PSConsoleThemeDebugSessionPath} 'PSConsoleTheme.psd1') -Force
+            Import-Module (Join-Path $targetDir 'PSConsoleTheme.psd1') -Force
         }
         Release {
             $paths = $env:PSModulePath -split ';' | Where-Object { $_ -like "${env:USERPROFILE}*" }
@@ -86,7 +55,6 @@ task Install LayoutModule, {
 $layoutModuleParams = @{
     Inputs = {
         Get-ChildItem `
-            PSConsoleTheme/Lib/bin/$Configuration/PSConsoleTheme.dll, # needs to be processed before module manifest (.psd1)
             PSConsoleTheme/*.ps*,
             PSConsoleTheme/Private/*.ps1,
             PSConsoleTheme/Public/*.ps1,
@@ -94,39 +62,21 @@ $layoutModuleParams = @{
     }
     Outputs = {
         process {
-            if ((Split-Path $_ -Leaf) -eq 'psconsoletheme.dll') {
-                Join-Path $targetDir (Split-Path $_ -Leaf)
-            } else {
-                Join-Path $targetDir ($_ -replace [regex]::Escape($BuildRoot + '\PSConsoleTheme\'), '')
-            }
+            Join-Path $targetDir ($_ -replace [regex]::Escape($BuildRoot + '\PSConsoleTheme\'), '')
         }
     }
 }
 
 # Synopsis: Copy all of the files that belong in the module to one place for installation
-task LayoutModule -Partial @layoutModuleParams BuildBinaryModule, BuildMamlHelp, {
+task LayoutModule -Partial @layoutModuleParams BuildMamlHelp, {
     process {
         if (-Not (Test-Path (Split-Path $2) -PathType Container)) {
             New-Item (Split-Path $2) -ItemType Directory -Force | Out-Null
         }
-
-        if ((Split-Path $_ -Leaf) -eq 'psconsoletheme.dll') {
-            Write-Verbose "Copying $($_ -replace [regex]::Escape($BuildRoot + '\'), '') -> $($2 -replace [regex]::Escape($BuildRoot + '\'), '')"
-            try {
-                Copy-Item $_ $2 -Force -ErrorAction Stop
-            }
-            catch {
-                if ($Configuration -eq 'Debug') {
-                    $Global:PSConsoleThemeDebugSessionPath = New-DebugTarget
-                    throw "Build changes require a new target directory. Please run Invoke-Build again."
-                }
-            }
-        } else {
             Write-Verbose "Copying $($_ -replace [regex]::Escape($BuildRoot + '\'), '') -> $($2 -replace [regex]::Escape($BuildRoot + '\'), '')"
             Copy-Item $_ $2 -Force
         }
     }
-}
 
 # Synopsis: Publish the module to PSGallery
 task Publish -If ($Configuration -eq 'Release') {
